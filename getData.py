2db64d245448589afa4d4ce802a1ff5a9e03723d
Yakov-Bader <yakovbader@gmail.com>
2022-03-13 19:12:00 +0200

    start sql

---
+++ getData.py
@@ -0,0 +1,13 @@
+import mysql.connector as sql
+
+connection = sql.connect(host='localhost',
+                                         database='Electronics',
+                                         user='pynative',
+                                         password='pynative@#29')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
